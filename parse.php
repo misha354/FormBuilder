933d0644b5a2c1973c0ef5ec38a33d52a114281d
Mike Zhukovskiy <misha354@gmail.com>
2016-04-12 20:33:49 -0400

    Revert the test from test condition to previous state

--- parse.php
+++ parse.php
@@ -8,8 +8,7 @@ if (!file_exists($argv[1])){
 	die("File $argv[1] doesn't exist.");
 }
 
-//$lexer = new FormLexer(file_get_contents($argv[1])); // parse command-line arg
-$lexer = new FormLexer(""); // parse command-line arg
+$lexer = new FormLexer(file_get_contents($argv[1])); // parse command-line arg
 
 $parser = new FormParser($lexer);
 $parser->form_elements(); // begin parsing at rule list
